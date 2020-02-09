baa12938ee700ffae97cb81fb68ba014a7335ecf
Saiyueze <gavinhwa@163.com>
2020-02-10 01:20:29 +0800

    add chapter-simple-1 project
    Signed-off-by: Saiyueze <gavinhwa@163.com>

---
+++ 2.2.4/chapter-simple-1/src/test/java/com/saiyueze/chaptersimple1/ChapterSimple1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiyueze.chaptersimple1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterSimple1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4b303d856c3cd912d7185cb8d8ab303e33f354df
buffer <gavinhwa@qq.com>
2020-02-10 01:04:09 +0800

    add chapter-simple-1 project and update .gitignore file.

---
+++ 2.2.x/chapter-simple-1/src/main/java/com/saiyueze/chaptersimple1/ChapterSimple1Application.java
@@ -0,0 +1,13 @@
+package com.saiyueze.chaptersimple1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterSimple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterSimple1Application.class, args);
+	}
+
+}

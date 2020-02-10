704e4971234fcfc4d2a841806f97b13869f2d03f
Saiyueze <gavinhwa@163.com>
2020-02-10 13:41:57 +0800

    remove unused references.

--- 2.2.4/chapter-simple-1/src/main/java/com/saiyueze/chapter/simple1/ChapterSimple1Application.java
+++ 2.2.4/chapter-simple-1/src/main/java/com/saiyueze/chapter/simple1/ChapterSimple1Application.java
@@ -2,8 +2,6 @@ package com.saiyueze.chapter.simple1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ChapterSimple1Application {
@@ -11,5 +9,4 @@ public class ChapterSimple1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ChapterSimple1Application.class, args);
 	}
-
 }
